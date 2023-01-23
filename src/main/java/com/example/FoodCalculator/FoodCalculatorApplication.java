2625e5cac0f40304e2dde892a4b434e6d64937fb
Barchynai <barchynai.iakubova@gmail.com>
2023-01-23 15:25:25 +0600

    project

---
+++ src/main/java/com/example/FoodCalculator/FoodCalculatorApplication.java
@@ -0,0 +1,14 @@
+package com.example.FoodCalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodCalculatorApplication.class, args);
+		System.out.println("Welcome, project name is Food Calculator");
+	}
+
+}
---
+++ src/test/java/com/example/FoodCalculator/FoodCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FoodCalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
